20c5df1d5131b479e892e274323a649f21eb6d3a
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-11-03 09:47:55 +0200

    feat: create solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const path = require('path');
+const fs = require('fs');
 
+let filePath = path.resolve(__dirname, 'files-copy');
+let fileOrigin = path.resolve(__dirname, 'files');
+
+fs.mkdir(filePath, {recursive: true}, (err, path1) => {
+    fs.readdir(filePath, (err, files) => {
+        if (err) return;
+
+        for (const file of files) {
+            fs.unlink(path.join(filePath, file), (err) => {
+                if (err) return;
+            });
+        }
+    });
+
+    fs.readdir(fileOrigin, (err, files) => {
+        if (err) return;
+
+        for (const file of files) {
+            fs.copyFile(path.join(fileOrigin, file), path.join(filePath, file), (err) => {
+                if (err) return;
+            });
+        }
+    });
+})
+
+
+//fs.copyFile
